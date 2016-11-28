f176eacf2a0237050eb812ece1bfd300025b7dd8
wu <954358350@qq.com>
2016-11-28 17:10:35 +0800

    wu

--- HomeWork/HomeWork/Controllers/StudentController.cs
+++ HomeWork/HomeWork/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HomeWork.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,13 +9,47 @@ namespace HomeWork.Controllers
 {
     public class StudentController : Controller
     {
+        BLL.Student student = new BLL.Student();
         //
         // GET: /Student/
 
         public ActionResult Index()
         {
+            List<Chapter> chap = student.selectChapter();
+            SelectList list = new SelectList(chap, "ChapterId", "ChapterName");
+            ViewBag.list = list;
             return View();
         }
+        public ActionResult List()
+        {
+            return PartialView("List");
+        }
+        public ActionResult Yuxi()
+        {
+            //if (homeworkType == 1)
+            //{
+            //    //List<QueryHomeWork> list = student.selectYuxi(studentNo, chapterId);
+            //    //ViewBag.yuxi = list;
+            //    return PartialView("Yuxi");
+            //}
+            //if (homeworkType == 2)
+            //{
+            //    return PartialView("Yuxi");
+            //}
+            //if (homeworkType == 3)
+            //{
+            //    return PartialView("Yuxi");
+            //}
 
+            return PartialView("Yuxi");
+        }
+        public ActionResult Yuntiku()
+        {
+            return PartialView("Yuntiku");
+        }
+        public ActionResult Lianxi()
+        {
+            return PartialView("Lianxi");
+        }
     }
 }
---
+++ HomeWork/HomeWork/Models/SelectListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeWork.Models
+{
+    public class SelectListItem
+    {
+        //public SelectListItem();
+        public bool Selected { get; set; }
+        public string Text { get; set; }
+        public string Value { get; set; }
+    }
+}
